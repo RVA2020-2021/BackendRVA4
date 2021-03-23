5dd8c4e6a855004bfada23712040f137d179faac
user <user@DESKTOP-HJGV6OB>
2021-03-23 10:35:58 +0100

    Cetvrte vezbe

--- BackendRVA4/src/main/java/rva/ctrls/ArtiklRestController.java
+++ BackendRVA4/src/main/java/rva/ctrls/ArtiklRestController.java
@@ -3,8 +3,15 @@ package rva.ctrls;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rva.jpa.Artikl;
@@ -12,23 +19,61 @@ import rva.repository.ArtiklRepository;
 
 @RestController
 public class ArtiklRestController {
-	
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	@Autowired
 	private ArtiklRepository artiklRepository;
-	
+
 	@GetMapping("artikl")
-	public Collection<Artikl> getArtikli(){
+	public Collection<Artikl> getArtikli() {
 		return artiklRepository.findAll();
 	}
-	
+
 	@GetMapping("artikl/{id}")
-	public Artikl getArtikl(@PathVariable ("id") Integer id){
+	public Artikl getArtikl(@PathVariable("id") Integer id) {
 		return artiklRepository.getOne(id);
 	}
-	
+
 	@GetMapping("artiklNaziv/{naziv}")
-	public Collection<Artikl> getArtiklByNaziv(@PathVariable ("naziv") String naziv){
+	public Collection<Artikl> getArtiklByNaziv(@PathVariable("naziv") String naziv) {
 		return artiklRepository.findByNazivContainingIgnoreCase(naziv);
+	}
+
+	@PostMapping("artikl")
+	public ResponseEntity<Artikl> insertArtikl(@RequestBody Artikl artikl) {
+
+		if (!artiklRepository.existsById(artikl.getId())) {
+			artiklRepository.save(artikl);
+			return new ResponseEntity<Artikl>(HttpStatus.OK);
+		}
+		return new ResponseEntity<Artikl>(HttpStatus.CONFLICT);
+
+	}
+
+	@PutMapping("artikl")
+	public ResponseEntity<Artikl> updateArtikl(@RequestBody Artikl artikl) {
+
+		if (!artiklRepository.existsById(artikl.getId())) {
+			return new ResponseEntity<Artikl>(HttpStatus.NO_CONTENT);
+		}
+		artiklRepository.save(artikl);
+		return new ResponseEntity<Artikl>(HttpStatus.OK);
+	}
+
+	@DeleteMapping("artikl/{id}")
+	public ResponseEntity<Artikl> deleteArtikl(@PathVariable("id") Integer id) {
+
+		if (!artiklRepository.existsById(id)) {
+			return new ResponseEntity<Artikl>(HttpStatus.NO_CONTENT);
+		}
+		artiklRepository.deleteById(id);
+
+		if (id == -100) {
+			jdbcTemplate.execute("insert into \"artikl\" (\"id\", \"naziv\", \"proizvodjac\") values (-100, 'Jegoda test', 'Proizvodjac test')");
+		}
+		return new ResponseEntity<Artikl>(HttpStatus.OK);
 	}
 
 }
